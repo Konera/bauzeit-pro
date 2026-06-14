e296b19177d18083b692eacbf5a49ff381d47b8f
Konera <konera@users.noreply.github.com>
2026-06-14 17:00:41 +0200

    🔄 Samsung WebView Cache-Fix
    - MainActivity: clearCache(true) + LOAD_NO_CACHE bei jedem Start
    - Samsung WebView zeigte alten Code aus dem Cache
    - Jetzt wird immer der aktuelle Code aus den Assets geladen

--- android/app/src/main/java/de/konera/bauzeitpro/MainActivity.java
+++ android/app/src/main/java/de/konera/bauzeitpro/MainActivity.java
@@ -1,5 +1,24 @@
 package de.konera.bauzeitpro;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // WebView-Cache leeren bei jedem Start
+        // Damit wird immer der aktuelle Code aus den Assets geladen
+        try {
+            WebView webView = getBridge().getWebView();
+            if (webView != null) {
+                webView.clearCache(true);
+                webView.getSettings().setCacheMode(android.webkit.WebSettings.LOAD_NO_CACHE);
+            }
+        } catch (Exception e) {
+            // Non-blocking: Cache-Clearing darf App nicht crashen
+        }
+    }
+}
